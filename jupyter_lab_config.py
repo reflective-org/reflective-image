a8183eaf3623e1caa0a72db135a133d1290935d8
John Orcutt <johnorcutt@users.noreply.github.com>
2026-08-05 18:33:04 -0700

    Update the branding on the hub to more closely match reflective's (#7)
    * testing branding updates
    
    * touch ups
    
    * minor tweaks, removed animation on splash screen and use favicon with no background
    
    * larger favicon
    
    * remove the jupyter animation on the splash screen

---
+++ jupyter_lab_config.py
@@ -0,0 +1,6 @@
+c.Branding.logo_uri = "/opt/reflective/images/Monogram-Yellow.svg"
+c.Branding.splash_logo_uri = "/opt/reflective/images/Monogram-Yellow.svg"
+c.Branding.short_name = "Cloud Hub"
+
+# Load custom/custom.css so we can enlarge the top-bar logo.
+c.LabApp.custom_css = True
